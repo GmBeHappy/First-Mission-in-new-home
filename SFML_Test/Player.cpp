4dec562e1cebdf357ad58a4b555daa9ca7365e3f
GmBeHappy <gmpplk@hotmail.com>
2020-12-14 18:08:53 +0700

    update player

--- SFML_Test/Player.cpp
+++ SFML_Test/Player.cpp
@@ -1,14 +1,34 @@
 #include "Player.h"
 
-void Player::initialBulletTexture()
+void Player::initialBodyHitbox()
+{
+	this->bodyHitbox.setSize(sf::Vector2f(85.0f, 105.0f));
+	this->bodyHitbox.setOutlineColor(sf::Color::Green);
+	this->bodyHitbox.setFillColor(sf::Color::Transparent);
+	this->bodyHitbox.setOutlineThickness(2.0f);
+}
+
+void Player::updateHitbox()
+{
+	this->bodyHitbox.setPosition(sf::Vector2f(this->body.getPosition().x - 50.0f ,this->body.getPosition().y - 50.0f));
+}
+
+void Player::initialVariables()
 {
 	
+	// attack per time
+	this->attackCooldownMax = 10.f;
+	this->attackCooldown = this->attackCooldownMax;
+
+	// HP
+	this->hpMax = 100;
+	this->hp = this->hpMax;
 }
 
 void Player::initObject()
 {
-	this->body.setOutlineColor(sf::Color::Green);
-	this->body.setOutlineThickness(2.0f);
+	//this->body.setOutlineColor(sf::Color::Green);
+	//this->body.setOutlineThickness(2.0f);
 	this->spaceship.setSize(sf::Vector2f(245.0f, 255.0f));
 	this->spaceship.setOutlineColor(sf::Color::Green);
 	this->spaceship.setFillColor(sf::Color::Transparent);
@@ -16,6 +36,71 @@ void Player::initObject()
 	this->spaceship.setPosition(sf::Vector2f(1490.0f, 2100.0f));
 }
 
+void Player::updateMovement(float deltatime)
+{
+	// update movement
+	sf::Vector2f movement(0.0f, 0.0f);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+		if (body.getPosition().x > 0) {
+			movement.x -= movementSpeed * deltatime;
+		}
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+		if (body.getPosition().x < 7521) {
+			movement.x += movementSpeed * deltatime;
+		}
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+		if (body.getPosition().y > 0) {
+			movement.y -= movementSpeed * deltatime;
+		}
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+		if (body.getPosition().y < 4234) {
+			movement.y += movementSpeed * deltatime;
+		}
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) and sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
+		if (body.getPosition().x > 0) {
+			movement.x -= 130 * deltatime;
+		}
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) and sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
+		if (body.getPosition().x < 7521) {
+			movement.x += 130 * deltatime;
+		}
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) and sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
+		if (body.getPosition().y > 0) {
+			movement.y -= 130 * deltatime;
+		}
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) and sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
+		if (body.getPosition().y < 4234) {
+			movement.y += 130 * deltatime;
+		}
+	}
+
+	// animation
+	if (movement.x == 0.0f) { // IDLE
+		row = 0;
+	}
+	else { // walking ar running
+		row = 1;
+		if (movement.x > 0.0f) {
+			faceRight = true;  // face right
+		}
+		else {
+			faceRight = false; // face left
+		}
+	}
+	// update animation
+	animation.Update(row, deltatime, faceRight);
+	body.setTextureRect(animation.uvRect);
+
+	// update player movement
+	body.move(movement);
+}
 
 Player::Player(sf::RenderWindow* window,sf::Texture* &texture, sf::Vector2u imageCount, float switchTime, float speed, sf::Mouse* mouse): 
 	animation(texture, imageCount, switchTime)
@@ -24,112 +109,108 @@ Player::Player(sf::RenderWindow* window,sf::Texture* &texture, sf::Vector2u imag
 	row = 0;
 	faceRight = true;
 
+	// initial player
 	body.setSize(sf::Vector2f(150.0f, 150.0f));
 	body.setOrigin(body.getSize()/2.0f);
 	body.setPosition(this->spawnPoint);
-	body.setFillColor(sf::Color::Red);
+	body.setTexture(texture);
 	
-	this->clock = new sf::Clock;
-	this->time = new sf::Time;
-
-	this->initialBulletTexture();
 	this->initObject();
+	this->initialBodyHitbox();
 	
 	this->window = window;
 	this->mouse = mouse;
-	//body.setOrigin(sf::Vector2f(-250.0f, -450.0f));
 
-	body.setTexture(texture);
-	
 }
-
 
 Player:: ~Player()
 {
 	
 }
 
+const sf::Vector2f& Player::getPos() const
+{
+	return this->body.getPosition();
+}
+
+sf::FloatRect Player::getBounds() const
+{
+	return this->bodyHitbox.getGlobalBounds();
+}
+
+const int& Player::getHp() const
+{
+	return this->hp;
+}
+
+const int& Player::getHpMax() const
+{
+	return this->hpMax;
+}
+
+void Player::setPosition(const sf::Vector2f pos)
+{
+	this->body.setPosition(pos);
+}
+
+void Player::setPosition(const float x, float y)
+{
+	this->body.setPosition(x, y);
+}
+
+void Player::setHP(const int hp)
+{
+	this->hp = hp;
+}
+
+void Player::loseHP(const int value)
+{
+	this->hp -= value;
+	if (this->hp < 0)
+	{
+		this->hp = 0;
+	}
+
+	if (this->hp > 100)
+	{
+		this->hp = 100;
+	}
+}
+
+const bool Player::canAttack()
+{
+	if (this->attackCooldown >= this->attackCooldownMax)
+	{
+		this->attackCooldown = 0.f;
+		return true;
+	}
+	return false;
+}
+
+void Player::updateAttack()
+{
+	if (this->attackCooldown < this->attackCooldownMax)
+	{
+		this->attackCooldown += 0.25f;
+	}
+}
 
 void Player::update(float deltatime)
 {
-	sf::Vector2f movement(0.0f, 0.0f);
-	this->time = &this->clock->getElapsedTime();
-	this->showtime = this->time->asSeconds();
-	
-	//printf("%llu\n", this->showtime);
+	this->updateMovement(deltatime);
+	this->updateAttack();
+	this->updateHitbox();
+
+	// simple collision check
 	if (!body.getGlobalBounds().intersects(this->spaceship.getGlobalBounds())) {
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-			if (body.getPosition().x > 0) {
-				movement.x -= movementSpeed * deltatime;
-			}
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-			if (body.getPosition().x < 7521) {
-				movement.x += movementSpeed * deltatime;
-			}
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-			if (body.getPosition().y > 0) {
-				movement.y -= movementSpeed * deltatime;
-			}
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-			if (body.getPosition().y < 4234) {
-				movement.y += movementSpeed * deltatime;
-			}
-		}
-
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) and sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
-			if (body.getPosition().x > 0) {
-				movement.x -= 130 * deltatime;
-			}
-		}
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) and sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
-			if (body.getPosition().x < 7521) {
-				movement.x += 130 * deltatime;
-			}
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) and sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
-			if (body.getPosition().y > 0) {
-				movement.y -= 130 * deltatime;
-			}
-		}
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) and sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
-			if (body.getPosition().y < 4234) {
-				movement.y += 130 * deltatime;
-			}
-		}
+		printf("Collision!!\n");
 	}
-
-	if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-		
-		
-	}
-
-	if (movement.x == 0.0f) {
-		row = 0;
-	}
-	else {
-		row = 1;
-		if (movement.x > 0.0f) {
-			faceRight = true;
-		}
-		else {
-			faceRight = false;
-		}
-	}
-	animation.Update(row, deltatime, faceRight);
-	body.setTextureRect(animation.uvRect);
-	body.move(movement);
 }
 
 void Player::render(sf::RenderTarget& target)
 {
 	target.draw(this->spaceship);
+	target.draw(this->bodyHitbox);
 	target.draw(body);
 	
 }
--- SFML_Test/Player.h
+++ SFML_Test/Player.h
@@ -2,56 +2,73 @@
 #include<SFML/Graphics.hpp>
 #include<SFML/System.hpp>
 #include "Animation.h"
-#include "Bullet.h"
 #include <iostream>
 #include <map>
 
 class Player
 {
 private:
+	// player model
 	sf::RectangleShape body;
+	sf::RectangleShape bodyHitbox;
 	sf::RectangleShape spaceship;
-	sf::Clock* clock;
-	sf::Time* time;
 	Animation animation;
-	Bullet* b1;
 
+	// player variable
 	sf::Vector2f spawnPoint = { 1850.0f, 2200.0f };
 
-	std::vector<Bullet> bullets;
-	
+	// HP
+	int hp;
+	int hpMax;
 
-	sf::Texture* bulletTexture;
+	// attack & max speed
+	float attackCooldown;
+	float attackCooldownMax;
 
-	sf::Vector2f playerCenter;
-	sf::Vector2f mousePosition;
-	sf::Vector2f aimDirection;
-	sf::Vector2f aimDirectionNormalize;
-
+	// Mouse
 	sf::Mouse* mouse;
 	
+	// Window
 	sf::RenderWindow* window;
-	//std::ostringstream showtime;
-	unsigned long long showtime;
+	
+	// Movement
 	unsigned int row;
 	float movementSpeed;
 	bool faceRight;
 	float suitPower=2000;
+	
 	/*private function*/
-	void initialBulletTexture();
+	void initialBodyHitbox();
+	void updateHitbox();
+	void initialVariables();
 	void initObject();
+	void updateMovement(float deltatime);
 	
 
 public:
 	Player(sf::RenderWindow* window, sf::Texture* &texture, sf::Vector2u imageCount, float switchTime, float speed, sf::Mouse* mouse);
-	
-	
+	virtual ~Player();
+
+	// Access
+	const sf::Vector2f& getPos() const;
+	sf::FloatRect getBounds() const;
+	const int& getHp() const;
+	const int& getHpMax() const;
 	sf::Vector2f GetPosition() {
 		return body.getPosition();
 	}
 
-	virtual ~Player();
+	// modify
+	void setPosition(const sf::Vector2f pos);
+	void setPosition(const float x, float y);
+	void setHP(const int hp);
+	void loseHP(const int value);
 
+	// attack
+	const bool canAttack();
+	void updateAttack();
+	
+	// update and render
 	void update(float deltatime);
 	void render(sf::RenderTarget& target);
 
